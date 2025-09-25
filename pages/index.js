aa4f1f5a1fa4c6c7ca66b9955f20811c1df85bf8
LeQ <47932446+LeXTea@users.noreply.github.com>
2025-09-25 11:09:35 -0700

    wip weather display

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,24 @@ export default function Home() {
     await sdk.actions.ready()
   })()
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <div className="container-fluid">
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Leo's Simple Weather" />
+        <section className="inputs">
+          <input type="text" id="cityInput" placeholder="Enter city name" />
+          <button id="getWeatherBtn">Get Weather</button>
+          <div id="weatherResult"></div>
+        </section>
+        <section className="display">
+          <div className="wrapper">
+            <h2 id="cityOutput"></h2>
+            <p id="description"></p>
+            <p id="temp"></p>
+            <p id="wind"></p>
+          </div>
+        </section>
+        <Footer />
       </main>
-
-      <Footer />
     </div>
   )
 }
