f512e36a1b8f1c4061e4ab6bdce78db41298743b
LeQ <47932446+LeXTea@users.noreply.github.com>
2025-09-25 17:06:51 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01998356-d908-843e-bf0b-9153bae03cd1',
+        permanent: false,
+      },
+    ]
+  },
+}
